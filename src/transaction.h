309e22da6cddc6b605f940882ae145f4a685707b
John Steele <programjsteele@gmail.com>
2010-11-11 12:06:57 -0800

    Added some general documentation and method signitures. Still unsure on a few things.

---
+++ src/transaction.h
@@ -0,0 +1,44 @@
+/**
+ * @file transaction.h 
+ * 
+ * @brief 
+ *
+ * @brief CSS 343 - Lab 4
+ *
+ * @author John Steele  \<steelejr@u.washington.edu\> 
+ *  
+ * @version 1.0.0
+ * @date November 9, 2010
+ */ 
+//--------------------------------------------------------------------
+/**
+ * Includes following features:
+ * 	- 
+ * 
+ * Assumptions:
+ * 	-  
+ */
+//--------------------------------------------------------------------
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+
+/** 
+ * @class Transaction
+ */
+class Transaction {
+
+/**
+ * @public
+ */
+public:
+
+/**
+ * @private
+ */
+private:
+
+};
+#endif /* TRANSACTION_H */
+

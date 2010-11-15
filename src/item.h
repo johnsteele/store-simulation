4e78cbdf46e64361e4d31494849e657117eb3930
John Steele <programjsteele@gmail.com>
2010-11-15 10:32:37 -0800

    Completed the documentation and methods.

--- src/item.h
+++ src/item.h
@@ -3,8 +3,7 @@
  * 
  * @brief An Item is an abstract base class that can be inherited to
  *	  share a common interface type and allow poloymorphic
- *	  behavior of the different Item types. This class cannot be
- *	  instantiated.
+ *	  behavior of the different Item types. 
  *
  * @brief CSS 343 - Lab 4
  *
@@ -17,17 +16,17 @@
 /**
  * Includes following features:
  * 	- Allows clients to create a new instance of a derived type 
- *	  by calling getNewInstance. getNewInstance will be 
- *	  called on the derived type, thus allowing dynamic binding.
- *	  The data of the new object will be set using the provided
- *	  file stream.
+ *	  by calling getNewInstance. 
+ * 	- Allows clients to compare Items for equality.
+ * 	- Allows clients to compare Items for less than value.
+ *	- Allows clients to print the Item.
  * 
  * Assumptions:
- * 	- All derived types of Item implement getNewInstance, and 
+ * 	- All derived classes of Item implement getNewInstance, and 
  *	  within the method return a dynamically allocated object of
- *	  the derived type. The data of the new object will be set
- *	  using the provided file stream, so it must be open and 
- *	  correctly formatted according to Lab 4 specifications.
+ *	  the derived type. 
+ *	- The file stream passed to createNewInstance is open and 
+ *	  constains correct format according to Lab 4 specifications.
  */
 //--------------------------------------------------------------------
 
@@ -35,6 +34,7 @@
 #define ITEM_H
 
 #include <fstream> 
+#include <string>
 #include <iostream>
 
 /**
@@ -47,28 +47,48 @@ using namespace std;
  */
 class Item {
 
-	//---------------------operator<<-----------------------------
-	/**
-	 * @brief Overloaded operator<<. Sends the data of this Item
- 	 *	  to the provided output stream. 
- 	 */
-	friend ostream& operator<< (ostream &, const Item &); 
-
 /**
  * @public
  */
 public:
 
-	//---------------------Constructor----------------------------
+	//---------------------Default Constructor--------------------
 	/**
  	 * @brief Creates an Item object with default values.
+	 *
+	 * Preconditions: None.
+	 *
+	 * Postconditions: The data members of this Item have been 
+	 *		   initialized with default values.
  	 */
 	Item ();
 
 
+	//---------------------Constructor----------------------------
+	/**
+ 	 * @brief Creates an Item object with the specified values.
+	 *
+	 * Preconditions: None.
+	 *
+	 * Postconditions: The data members of this Item have been set
+	 *		   to the specified values.
+	 *
+	 * @param type  The type of Item.
+	 * @param name  The name of this Item.
+	 * @param title The title of this Item.
+	 * @param year  The year of this Item.
+ 	 */
+	Item (char type, string name, string title, int year);
+
+
 	//---------------------Destructor-----------------------------
 	/**
- 	 * @brief Deletes any dynamically allocated memory. 
+ 	 * @brief Deletes all dynamically allocated memory. 
+	 *
+ 	 * Preconditions: my_name and my_title are NULL or point to
+	 *		  dynamically allocated data.
+	 *
+ 	 * Postconditions: my_name and my_title have been deleted.
 	 */
 	virtual ~Item ();
 
@@ -80,46 +100,140 @@ public:
  	 *	  the provided file stream.
 	 *	
  	 * Preconditions: The file stream is open and correctly 
-	 *		  formatted according to Lab 4 specs.
-	 *	
+	 *		  formatted according to Lab 4 specs.  *	
 	 * Postconditions: A new object of the derived type is created
 	 *		   using the provided file to set its data.	
  	 *		   A pointer to the object is then returned.
 	 *
+	 * @param intput The input stream to extract from.
 	 * @return Returns a pointer to a  new instance of the derived
 	 *	   Item.
 	 */
-	virtual Item * getNewInstance (ifstream &) const = 0;
+	virtual Item * getNewInstance (ifstream &input) const = 0;
+
 	
+	//---------------------operator<------------------------------
+	/**
+	 * @brief Compares this Item with the_other for less than 
+	 *	  value.
+	 *
+	 * Preconditions: The data members of this Item have been 
+	 *	  	  initialized.
+ 	 *
+	 * Prostconditions: Returns true if this Item is less than
+	 *		    the_other.
+	 *	 
+ 	 * @param the_other The other Item to compare with this Item. 
+	 * @return Returns true if this Item is less than the_other, 
+	 *	   false otherwise. 
+ 	 */
+	virtual bool operator< (const Item &the_other) const = 0; 
+
+
+	//---------------------operator==-----------------------------
+	/**
+	 * @brief Compares this Item with the_other Item. 
+	 *
+	 * Preconditions: The data members of this Item have been 
+	 *		  initialized.
+	 *
+	 * Postconditions: Returns true if this Item is equal to 
+	 *		   the_other.
+	 *
+	 * @param the_other The other Item to compare with this Item.
+	 * @return Returns true if the_other is equal to this Item, 
+	 *	   false otherwise.
+ 	 */
+	virtual bool operator== (const Item &the_other) const = 0; 
+
+
+	//---------------------print----------------------------------
+	/** 
+	 * @brief Prints the data members of this Item to the output
+	 *	  stream.
+	 * 
+	 * Preconditions: my_name and my_title have a terminating 
+	 *		  character at the end of their strings.
+	 *
+	 * Postconditions: my_type, my_count, my_name, my_title, 
+	 *		   and my_year have been printed to the 
+	 *		   output stream.
+ 	 */
+	virtual void print () const; 
+
+
+/**
+ * @protected
+ */
+protected:
+	
+	//---------------------getName--------------------------------
+	/**
+	 * @brief An accessor method for derived classes to use for 
+	 * 	  comparisons.
+	 *
+	 * Preconditions: my_name is initialized.
+	 *
+	 * Postconditions: my_name is returned.
+	 *
+	 * @return Returns the name of this Item.	
+	 */
+	const string getName () const;
+
+	
+	//---------------------getTitle-------------------------------
+	/**
+	 * @brief An accessor method for derived classe to use for 
+	 *	  comparisons.
+ 	 *
+	 * Preconditions: my_title is initialized.
+	 *
+ 	 * Postconditions: my_title is returned.
+	 * 
+	 * @return Returns the title of this Item.
+	 */	
+	const string getTitle () const;
+
+
+	//---------------------getYear--------------------------------
+	/**
+	 * @brief An accessor method for derived classe to use for 
+	 *	  comparisons.
+ 	 *
+	 * Preconditions: my_year is initialized.
+	 *
+ 	 * Postconditions: my_year is returned.
+	 * 
+	 * @return Returns the year of this Item.
+	 */	
+	int getYear () const;
+
+
 /**
  * @private
  */
 private:
 
-	/**
+-	/**
 	 * @brief The type of Item.
 	 */
 	char my_type;
 
 	/**
-	 * @brief The number of Items.
-	 */
-	int my_count;
-
-	/**
  	 * @brief The name or director of this Item.
 	 */
-	char *my_name;	
+	string my_name;	
 
 	/**
 	 * @brief The title of this Item.
 	 */
-	char *my_title;
+	string my_title;
 
 	/**
 	 * @brief The year of this Item.
 	 */	
 	int my_year;
+  
 };
 #endif /* ITEM_H */
 

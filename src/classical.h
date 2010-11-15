1f142f45a69910149a057411bf7de7e3bfe42197
John Steele <programjsteele@gmail.com>
2010-11-15 10:33:30 -0800

    Completet the documentation and methods.

--- src/classical.h
+++ src/classical.h
@@ -14,10 +14,12 @@
 //--------------------------------------------------------------------
 /**
  * Includes following features:
- * 	- Allows clients to create a new instance of Classical by 
- *	  by calling getNewInstance. getNewInstance will return a 
- * 	  pointer to a new Classical object with its data set using
- *	  the provided file stream.
+ * 	- Allows clients to get a pointer to a new instance of a 
+ *	  Classical by calling getNewInstance.
+ *	- Allows clients to compare Classical objects for equality.
+ *	- Allows clients to compare Classical objects for less than 
+	  value.
+ *	- Allows clients to print the Classical.
  * 
  * Assumptions:
  * 	- The file stream is open and correctly formatted according
@@ -40,44 +42,131 @@ class Classical: public CD {
  */
 public:
 
-	//---------------------Constructor----------------------------
+	//---------------------Default Constructor--------------------
 	/**
-	 * @brief Creates a Classical object with default values.
+	 * @brief Create a Classical object with default values. 
 	 *
 	 * Preconditions: None.
-	 *
-	 * Postconditions: This Classical object was instantiated with
-	 *		   default values.
- 	 */
+	 *	
+ 	 * Postconditions: This Classical was instantiated with 
+	 *		   default values. 
+	 */
 	Classical ();
 
 
-	//----------------------------Destructor----------------------
+	//---------------------Constructor----------------------------
 	/**
-	 * @brief Deletes all dynamic memory owned by this Classical
-	 *	  object.
+	 * @brief Creates a Classical object with the specified 
+	 *	  values.
 	 *
 	 * Preconditions: None.
+	 * 
+ 	 * Postconditions: The CD super class constructor was called
+ 	 *		   with the specified values, and the 
+	 *		   composer of this Classical was set. 
 	 *
-	 * Postconditions: All dynamic memory has been deleted.
+	 * @param type  The type of CD.
+	 * @param name  The name of the Classical.
+	 * @param title The title of the Classical. 
+	 * @param year  The year of the Classical.
+	 * @param composer The composer of the Classical. 
 	 */
-	virtual ~Classical (); 
+	Classical (char type, string name, 
+			string title, int year, string composer);		
 
 
-	//----------------------------getNewInstance------------------
+	//---------------------Destructor-----------------------------
 	/**
-	 * @brief Returns a pointer to a new Classical object with its
-	 *	  data set using the provided file stream.
+	 * @brief Deletes dynamically allocated memory.
 	 *
-	 * Preconditions: The file stream is open and correctly 
-	 *		  formatted according to Lab 4 specifications.
-	 *
-	 * Postconditions: A pointer to a new Classical object is 
-	 *		   returned. The data of the object was set
-	 *		   using the provided file stream. 
+	 * Preconditions: None.
+	 *	
+ 	 * Postconditions: my_composer was deleted and set to NULL.
 	 */
-	virtual Classical * getNewInstance (ifstream &) const;
+	virtual ~Classical ();
 
+		
+	//---------------------getNewInstance-------------------------
+	/**
+	 * @brief Retuns a pointer to a new Classical object CD. 
+	 *	  The objects' data will be set using the provided
+ 	 *	  file stream.
+	 *	
+ 	 * Preconditions: The file stream is open and correctly 
+	 *	 	  formatted according to Lab 4 specs.
+	 *	
+	 * Postconditions: A new Classical object was created using
+	 *		   the provided file stream, and a pointer
+	 *		   to it is returned. 
+	 *
+	 * @param input The input stream to extract from.
+	 * @return Returns a pointer to a new instance of a Classical.
+	 */
+	virtual Classical * getNewInstance (ifstream &input) const;
+
+
+	//---------------------operator<------------------------------
+	/**
+	 * @brief Compares this Classical CD with the_other for less 
+	 *	  than value. Comparisons are firt made with the 
+	 *	  composer, then by artist name, then by year, then by
+	 *	  title.
+	 *
+	 * Preconditions: The data members of the base class have 
+	 *		  been initialized, and accessor methods are 	
+ 	 *		  available from the base class to the members 
+	 *		  needed for comparisons.
+ 	 *
+	 * Prostconditions: Returns true if this Classical CD is less
+	 *		    than the_other.
+	 *	 
+ 	 * @param the_other The other Classical CD to compare with. 
+	 * @return Returns true if this Classical CD is less than 
+	 *	   the_other, false otherwise. 
+ 	 */
+	virtual bool operator< (const Item &the_other) const; 
+
+
+	//---------------------operator==-----------------------------
+	/**
+	 * @brief Compares this Classical CD with the_other Classical
+	 *	  CD. 
+	 *
+	 * Preconditions: The data members of the base class have
+ 	 *		  been initialized, and accessor methods are
+	 *		  available from the base class to the members
+	 *		  needed for comparisons.
+	 *
+	 * Postconditions: Returns true if this Classical CD is equal
+	 *		   to the_other.  
+	 *
+	 * @param the_other The other Classical CD to compare with 
+	 *		    this Classical CD.
+	 * @return Returns true if the_other is equal to this, false
+	 *		   otherwise.
+ 	 */
+	virtual bool operator== (const Item &the_other) const; 
+
+	
+	//---------------------print----------------------------------
+	/** 
+	 * @brief Prints the data members of this Classical CD in the
+ 	 * 	  output stream.
+	 * 
+	 * Preconditions: The data members of the base class have
+	 *		  been initialized.
+	 *
+	 * Postconditions: This Classical CD was printed in the output
+	 *	           stream. 
+ 	 */
+	virtual void print () const; 
+
+	
+/**
+ * @private
+ */
+private:
+	string my_composer;
 };
 #endif /* CLASSICAL_H */
  
--- src/rock.h
+++ src/rock.h
@@ -14,11 +14,12 @@
 //--------------------------------------------------------------------
 /**
  * Includes following features:
- * 	- Allows clients to create a new instance of a Rock cd by 
- *	  calling getNewInstance. getNewInstance will return a pointer
- * 	  to a newly created Rock cd object. The objects data will be
- *	  set using the provided file stream.
- * 
+ * 	- Allows clients to get a pointer to a new instance of a 
+ *	  Rock CD by calling getNewInstance.
+ *	- Allows clients to compare Rock CD's for equality.
+ *	- Allows clients to compare Rock CD's for less than value.
+ *	- Allows clients to print the Rock CD. 
+ *
  * Assumptions:
  * 	- The file stream is open and correctly formatted according to
  *	  Lab 4 specifications. 
@@ -40,36 +41,119 @@ class Rock: public CD {
  */
 public:
 
+	//---------------------Default Constructor--------------------
+	/**
+	 * @brief Create a Rock object with default values. 
+	 *
+	 * Preconditions: None.
+	 *	
+ 	 * Postconditions: This Rock CD was instantiated with 
+	 *		   default values. 
+	 */
+	Rock ();
+
+
 	//---------------------Constructor----------------------------
 	/**
- 	 * @brief Creates a Rock object with default values.
- 	 */
-	Rock ();
+	 * @brief Creates a Rock object with the specified 
+	 *	  values.
+	 *
+	 * Preconditions: None.
+	 * 
+ 	 * Postconditions: The CD super class constructor was called
+ 	 *		   with the specified values.
+	 *
+	 * @param type  The type of CD.
+	 * @param name  The name of the Rock CD.
+	 * @param title The title of the Rock CD. 
+	 * @param year  The year of the Rock CD.
+	 */
+	Rock (char type, string name, string title, int year);
 
 
 	//---------------------Destructor-----------------------------
 	/**
- 	 * @brief Deletes any dynamically allocated memory. 
+	 * @brief Deletes dynamically allocated memory.
+	 *
+	 * Preconditions: None.
+	 *	
+ 	 * Postconditions: All resources have been released. 
 	 */
 	virtual ~Rock ();
 
-	
+		
 	//---------------------getNewInstance-------------------------
 	/**
-	 * @brief Retuns a pointer to a new instance of a Rock object.
-	 *	  The data of the object will be set using the 
-	 *	  provided file stream.
-	 *
-	 * Preconditions: The file stream is open and correctly 
-	 *		  formatted according to Lab 4 secifications.
+	 * @brief Retuns a pointer to a new Rock object. 
+	 *	  The objects' data will be set using the provided
+ 	 *	  file stream.
 	 *	
- 	 * Postconditions: A pointer to a new Rock object is returned. 
-	 *		   The data of the object is set using the
-	 *		   provided file stream.
+ 	 * Preconditions: The file stream is open and correctly 
+	 *	 	  formatted according to Lab 4 specs.
+	 *	
+	 * Postconditions: A new Rock object was created using
+	 *		   the provided file stream, and a pointer
+	 *		   to it is returned. 
 	 *
-	 * @return Returns a pointer to a new Rock object.
+	 * @param input The input stream to extract from.
+	 * @return Returns a pointer to a new instance of a Rock.
 	 */
-	virtual Rock * getNewInstance (ifstream &) const;
+	virtual Rock * getNewInstance (ifstream &input) const;
+
+
+	//---------------------operator<------------------------------
+	/**
+	 * @brief Compares this Rock CD with the_other for less 
+	 *	  than value. Comparisons are first made by the
+	 *	  artist name, then by year, then by CD title.
+	 *
+	 * Preconditions: The data members of the base class have 
+	 *		  been initialized, and accessor methods are 	
+ 	 *		  available from the base class to the members 
+	 *		  needed for comparisons.
+ 	 *
+	 * Prostconditions: Returns true if this Rock CD is less
+	 *		    than the_other.
+	 *	 
+ 	 * @param the_other The other Rock CD to compare with. 
+	 * @return Returns true if this Rock CD is less than 
+	 *	   the_other, false otherwise. 
+ 	 */
+	virtual bool operator< (const Item &the_other) const; 
+
+
+	//---------------------operator==-----------------------------
+	/**
+	 * @brief Compares this Rock CD with the_other Rock CD. 
+	 *
+	 * Preconditions: The data members of the base class have
+ 	 *		  been initialized, and accessor methods are
+	 *		  available from the base class to the members
+	 *		  needed for comparisons.
+	 *
+	 * Postconditions: Returns true if this Rock CD is equal
+	 *		   to the_other.  
+	 *
+	 * @param the_other The other Rock CD to compare with this 
+	 *		    Rock CD.
+	 * @return Returns true if the_other is equal to this, false
+	 *		   otherwise.
+ 	 */
+	virtual bool operator== (const Item &the_other) const; 
+
+	
+	//---------------------print----------------------------------
+	/** 
+	 * @brief Prints the data members of this Rock CD in the
+ 	 * 	  output stream.
+	 * 
+	 * Preconditions: The data members of the base class have
+	 *		  been initialized.
+	 *
+	 * Postconditions: This Rock CD was printed in the output
+	 *	           stream. 
+ 	 */
+	virtual void print () const; 
 
 };
 #endif /* ROCK_H */
